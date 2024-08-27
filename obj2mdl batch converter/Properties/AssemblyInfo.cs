3b45e62926e09cf6d10af0a01b6059bef51768ca
Stanislav Vladev <stan0033@abv.bg>
2024-08-27 23:02:41 +0300

    1.0.4

--- obj2mdl batch converter/Properties/AssemblyInfo.cs
+++ obj2mdl batch converter/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
+ 
 using System.Runtime.InteropServices;
 using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("obj2mdl batch converter")]
+[assembly: AssemblyTitle("OBJ2MDL Batch Converter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("obj2mdl batch converter")]
+[assembly: AssemblyProduct("OBJ2MDL Batch Converter")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -43,5 +42,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyFileVersion("1.0.4")]
